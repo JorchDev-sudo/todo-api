06a6e75bf844c5742dc5c3c595169a0f88b448dc
JorchDev <jorgecoteralopez@gmail.com>
2025-12-20 15:00:36 -0600

    Implemented PostgreSQL and Docker with Flyway

--- src/main/java/com/jorchdev/todo_api/controllers/UserController.java
+++ src/main/java/com/jorchdev/todo_api/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/todo_api/users")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
     private final SecurityUtils securityUtils;
